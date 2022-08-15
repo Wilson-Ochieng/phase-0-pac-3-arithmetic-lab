efab7713cf5c294a0d9ae8689884a148512fa428
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-08-15 07:37:31 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,42 @@
+
+function add() {
+  return (number += 5);
+}
+
+function subtract() {
+  return (number -= 3);
+}
+function multiply() {
+  return (number -= 3);
+}
+function divide() {
+  return (number /= 2);
+}
+function add(a,b){
+  return (a+=b);
+}
+function subtract(a,b){
+  return (a -=b);
+}
+function multiply(a,b){
+  return (a *=b);
+}
+function divide(a,b){
+  return (a /=b);
+}
+function increment(n){
+  return n += 1;
+}
+function decrement(n){
+  return n -= 1;
+}
+function makeInt(n){
+  return parseInt(n,10);
+}
+function preserveDecimal(n){
+  return parseFloat(n);
+}
+add5();
+subtract3()
+multiplyBy3(); 
+divideBy2()
